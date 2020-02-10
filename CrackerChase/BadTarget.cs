892d566d4fa791ada21a5b8df531f11a12aa744a
Ben Marshall <ben321mars@gmail.com>
2020-02-10 15:36:07 +0000

    Created BarTarget.cs and added score to Target.cs

---
+++ CrackerChase/BadTarget.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrackerChase
+{
+    class BadTarget : Target
+    {
+        public BadTarget(int inScreenWidth, int inScreenHeight, Texture2D inSpriteTexture, int inDrawWidth, float inRestX, float inResetY) :
+            base(inScreenWidth, inScreenHeight, inSpriteTexture, inDrawWidth, inRestX, inResetY)
+        {
+            Score = -100;
+        }
+    }
+}
